bdd3a6cf5e8430756d43d462ab4747f322105a1c
Hongyuan Zhang <zhygwu2016@gmail.com>
2018-12-30 02:32:15 -0800

    20181228

--- week1/lost-in-tokyo-code/js/app.js
+++ week1/lost-in-tokyo-code/js/app.js
@@ -1,8 +1,20 @@
-//const Hello = props => <h1>Hello {props.name}</h1>;
+// const Hello = props => <h1>Hello {props.name}</h1>;
+// ReactDOM.render(<Hello name="Lawrence" />, document.getElementById('root'));
 
-//ReactDOM.render(<Hello name="Lawrence" />, document.getElementById('root'));
+const Title = props => <h1 className="tc">{props.title}</h1>;
 
-const Banner = props => <h1>Hello {props.firstName} {props.surName}</h1>;
+const Banner = props => (
+  <h1 className="tc f1 yellow pa3">
+    Hello {props.firstName} {props.surName}
+  </h1>
+);
 
-ReactDOM.render(<Banner firstName="Rik" surName="Lomas" />,
-document.getElementById('root'));
+const App = () => (
+  <div>
+    <Title title="Welcome to my website!" />
+    <Banner firstName="Lawrence" surName="Gosset" />
+  </div>
+);
+
+// ReactDOM.render(<Banner firstName="Rik" surName="Lomas" />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
