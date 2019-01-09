7d220029386c55001fb48680bd584b43420cccca
Hongyuan Zhang <zhygwu2016@gmail.com>
2019-01-08 23:56:56 -0800

    20190108

--- week1/lost-in-tokyo-code/js/app.js
+++ week1/lost-in-tokyo-code/js/app.js
@@ -1,3 +1,5 @@
+// http://superguides.netlify.com/react/jiffy/#jiffy-introduction
+
 /*
 // const Hello = props => <h1>Hello {props.name}</h1>;
 // ReactDOM.render(<Hello name="Lawrence" />, document.getElementById('root'));
